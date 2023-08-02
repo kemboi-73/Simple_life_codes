48d7665b378e43a107f2fde76323c064194b9b34
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2023-08-02 18:02:20 +0300

    calculates total amount of an harambee
    Total Contribution Calculator
    This Python script calculates the total contribution amount from a list of contributions made by individuals. The contributions are provided in the format of "Name Amount✅", where "Name" is the contributor's full name, "Amount" is the contribution amount (can be an integer or float), and "✅" indicates whether the contribution has a decimal point.

--- sum of contribution.py
+++ sum of contribution.py
@@ -8,32 +8,32 @@ def calculate_total_contributions(contributions_list):
 
     return total_contributions
 contributions_list = [
-    "Reuben Busienei 5550✅",
-    "Clementine koech 4700✅",
-    "marshal kiprop 4430✅",
-    "Silas Kitur 2030✅",
-    "Meshack kipkemboi 5230✅",
-    "Duncan kipchirchir 4000✅",
-    "Kiprop &KIPKURUI 3230✅",
-    "julia chepngetich 3030",
-    "Ruth kiplagat 5410✅",
-    "Derrick korir 3020✅",
-    "Robert kiptoo 2930✅",
-    "David keino 12100",
-    "Isaac Kitur 1900✅",
-    "Tuwei Hosea 1700✅",
-    "Silas sugut 3000✅",
-    "Nelly kirop 2000✅",
-    "Ismael kiptanui 1000✅",
-    "Justus samoei 2900✅",
-    "Amos kemboi 2000✅",
-    "Jennifer jepkirui 500✅",
-    "Nancy samoei 500✅",
-    "Tecla cheruyiot 500✅",
+    #the list
+    "1. Reuben Busienei 5550✅",
+    "2. Clementine koech 4700✅",
+    "3. marshal kiprop 4430✅",
+    "4. Silas Kitur 2030✅",
+    "5. Meshack kipkemboi 5230✅",
+    "6. Duncan kipchirchir 4000✅",
+    "7. Kiprop &KIPKURUI 3230✅",
+    "8. julia chepngetich 3030",
+    "9. Ruth kiplagat 5410✅",
+    "10. Derrick korir 3020✅",
+    "11. Robert kiptoo 2930✅",
+    "12. David keino 12100",
+    "13. Isaac Kitur 1900✅",
+    "14. Tuwei Hosea 1700✅",
+    "15. Silas sugut 3000✅",
+    "16. Nelly kirop 2000✅",
+    "17. Ismael kiptanui 1000✅",
+    "18. Justus samoei 2900✅",
+    "19. Amos kemboi 2000✅",
+    "20. Jennifer jepkirui 500✅",
+    "21. Nancy samoei 500✅",
+    "22. Tecla cheruyiot 500✅",
 ]
 
-# Calculate the total contributions
+# Total calculation
 total_contribution_amount = calculate_total_contributions(contributions_list)
-
-# Display the result
+#Results
 print("Total Contribution Amount:", total_contribution_amount)
